83975a341164f628f4e2ba28195a23f0ed4a5880
ShukriKastrati <shukki04@gmail.com>
2025-05-04 09:25:48 +0200

    fundi i projektit

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ bodyE1.addEventListener("mousemove", (event)=> {
     const xPos = event.offsetX;
     const ypos = event.offsetY;
     const spanE1 = document.createElement("span");
-    spanE1.style.left = xPos + "px"
-    spanE1.style.top = ypos + "px"
+    spanE1.style.left = xPos + "px";
+    spanE1.style.top = ypos + "px";
+    const size = Math.random() * 100;
+    spanE1.style.width = size + "px";
+    spanE1.style.height = size + "px";
     bodyE1.appendChild(spanE1);
     setTimeout(()=>{
         spanE1.remove();
